12e2d6f97c152027e45080ddf073e74d44df71a7
UaiSoh <joaopedroh101@gmail.com>
2025-09-21 11:07:42 -0300

    Class main alterada, super class Animal Alterada e as subclasses Cachorro e Gato criadas

--- src/main/java/br/edu/gitflow/Main.java
+++ src/main/java/br/edu/gitflow/Main.java
@@ -1,5 +1,29 @@
+
+import modelo.Cachorro;
+import modelo.Gato;
+
 public class Main{
   public static void main(String[] args){
+    
+    Cachorro cachorro = new Cachorro("Barto", 5, "Lulu da Pomerânia");
+
+    System.out.println(cachorro);
+    System.out.println();
+
+    cachorro.latir();
+
+    System.out.println();
+
+    Gato gato = new Gato("Bart", 3, "Laranja");
+
+    System.out.println(gato);
+    System.out.println();
+
+    gato.miar();
+    
+
+
+    
     
   }
 }
